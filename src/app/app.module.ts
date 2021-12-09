378d375e035a67bc2f9fd138349569a8db86392b
fabioemoutinho <fabioemoutinho@gmail.com>
2021-12-09 10:24:25 -0300

    adds ngrx dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,19 @@
-import {AppComponent} from './app.component';
-import {AppRoutingModule} from './app-routing.module';
-import {BrowserModule} from '@angular/platform-browser';
-import {environment as env} from '../environments/environment';
-import {AuthModule} from "@auth0/auth0-angular";
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { environment as env } from '../environments/environment';
+import { AuthModule } from '@auth0/auth0-angular';
 
-import {NglModule} from 'ng-lightning';
-import {LoginComponent} from './pages/login/login.component';
-import {HeaderComponent} from './core/header/header.component';
-import {HttpClientModule} from "@angular/common/http";
-import {NgModule} from '@angular/core';
-
+import { NglModule } from 'ng-lightning';
+import { LoginComponent } from './pages/login/login.component';
+import { HeaderComponent } from './core/header/header.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    HeaderComponent,
-  ],
+  declarations: [AppComponent, LoginComponent, HeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -27,10 +24,11 @@ import {NgModule} from '@angular/core';
       httpInterceptor: {
         ...env.httpInterceptor,
       },
-    })
+    }),
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: env.production }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
