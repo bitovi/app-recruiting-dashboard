c08adcee81d4437b3ab9a914ceb5cded0f1fd0b9
ishedu <idris@bitovi.com>
2021-12-16 09:35:54 -0500

    - changed variable name

--- src/app/dashboard/shared/pipe/label-to-array.pipe.ts
+++ src/app/dashboard/shared/pipe/label-to-array.pipe.ts
@@ -6,11 +6,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class LabelToArrayPipe implements PipeTransform {
   excludedWords = ['remote', 'Full', 'time', '&', 'and', ','];
 
-  transform(value: string, spiltTo: number): string[] {
+  transform(value: string, splitTo: number): string[] {
     if (value) {
       const regExp = new RegExp(this.excludedWords.join('\\b|\\b'), 'gi');
       const newLabels = value.replace(regExp, '').replace(/ +/g, ' ');
-      return newLabels.split(' ').slice(0, spiltTo);
+      return newLabels.split(' ').slice(0, splitTo);
     }
     return [];
   }
