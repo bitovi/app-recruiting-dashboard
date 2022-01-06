e39190b369c4add8df9cd44a870e9bcb812cd7d6
fabioemoutinho <fabioemoutinho@gmail.com>
2022-01-06 11:42:33 -0300

    rename folder dashboard-widget to widgets

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
 import { NglDatatablesModule, NglModule } from 'ng-lightning';
 import { NgChartsModule } from 'ng2-charts';
-import { PageFilterWidgetComponent } from './dashboard-widget/page-filter-widget/page-filter-widget.component';
+import { PageFilterWidgetComponent } from './widgets/page-filter-widget/page-filter-widget.component';
 import { WidgetsFilterComponent } from './shared/widgets-filter/widgets-filter.component';
 import { ComboboxComponent } from './shared/combobox/combobox.component';
 import { HeaderComponent } from './header/header.component';
@@ -18,11 +18,11 @@ import {
 } from '../shared/pipes';
 import { BRDDatePickerModule } from '../shared/components/date-picker/date-picker.module';
 import { ApplicantDetailsModule } from '../shared/components';
-import { WidgetWrapperComponent } from './dashboard-widget/widget-wrapper.component';
-import { WidgetRecruitingStageExitedComponent } from './dashboard-widget/widget-recruiting-stage-exited/widget-recruiting-stage-exited.component';
-import { WidgetJobsApplicantsComponent } from './dashboard-widget/widget-jobs-applicants/widget-jobs-applicants.component';
-import { WidgetNewApplicantsComponent } from './dashboard-widget/widget-new-applicants/widget-new-applicants.component';
-import { WidgetApplicantsTableComponent } from './dashboard-widget/widget-applicants-table/widget-applicants-table.component';
+import { WidgetWrapperComponent } from './widgets/widget-wrapper.component';
+import { WidgetRecruitingStageExitedComponent } from './widgets/widget-recruiting-stage-exited/widget-recruiting-stage-exited.component';
+import { WidgetJobsApplicantsComponent } from './widgets/widget-jobs-applicants/widget-jobs-applicants.component';
+import { WidgetNewApplicantsComponent } from './widgets/widget-new-applicants/widget-new-applicants.component';
+import { WidgetApplicantsTableComponent } from './widgets/widget-applicants-table/widget-applicants-table.component';
 
 const routes: Routes = [{ path: '', component: DashboardComponent }];
 
