ed7eb7ca975052d768926a4aaaf118e64aaebd50
Wade H <vdtdev@gmail.com>
2022-01-18 10:24:32 -0500

    Remove dnd placeholders

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,14 +1,11 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Observable } from 'rxjs';
 import { FilterDateState, FilterStore } from './store/filter.store';
 import { ApplicantsStore } from './store/applicants.store';
 import { ChartsStore } from './store/charts.store';
 import { JobsStore } from './store/jobs.store';
 import { WidgetConfig, WidgetLabels } from './widgets/widget.model';
-import {
-  CHART_WIDGET,
-  WidgetPanelModel,
-} from './widget-panel/widget-panel-model';
+import { WidgetPanelModel } from './widget-panel/widget-panel-model';
 import {
   WidgetDragAction,
   WidgetDragActionEvent,
@@ -20,11 +17,7 @@ import {
   styleUrls: ['./dashboard.component.scss'],
   providers: [FilterStore, ApplicantsStore, ChartsStore, JobsStore],
 })
-export class DashboardComponent implements AfterViewInit {
-  @ViewChild('placeholderBefore')
-  placeholderBeforeElement: ElementRef<HTMLDivElement>;
-  @ViewChild('placeholderAfter')
-  placeholderAfterElement: ElementRef<HTMLDivElement>;
+export class DashboardComponent {
   @ViewChild('gridSection') gridSectionElement: ElementRef<HTMLElement>;
 
   readonly totalApplicants$ = this.applicantsStore.totalApplicants$;
@@ -83,33 +76,12 @@ export class DashboardComponent implements AfterViewInit {
     },
   ];
 
-  public draggedWidget!: WidgetLabels;
+  draggedWidget!: WidgetLabels;
 
   constructor(
     private readonly filterStore: FilterStore,
     private readonly applicantsStore: ApplicantsStore
   ) {}
-
-  ngAfterViewInit(): void {
-    console.info({
-      before: this.placeholderBeforeElement,
-      after: this.placeholderAfterElement,
-      section: this.gridSectionElement,
-    });
-    this.injectPlaceholders(2);
-    this.injectPlaceholders(3);
-  }
-
-  injectPlaceholders(index: number, position?: number): void {
-    const gridElement = this.gridSectionElement.nativeElement;
-    const el = gridElement.querySelectorAll('brd-widget-wrapper')[index];
-    gridElement.insertBefore(this.placeholderBeforeElement.nativeElement, el);
-    el.insertAdjacentElement(
-      'afterend',
-      this.placeholderAfterElement.nativeElement
-    );
-    this.reArrangeWidgetItems(index, position);
-  }
 
   reArrangeWidgetItems(index: number, position: number) {
     const tempArray = [...this.widgetConfigs];
@@ -117,7 +89,7 @@ export class DashboardComponent implements AfterViewInit {
       (value) => value.component === this.draggedWidget
     );
     const draggedItemIndex = this.widgetConfigs.indexOf(draggedWidgetConfig);
-    const itemToBeReplaced = this.widgetConfigs[index];
+
     if (
       !this.isOpenedWidgetPanel ||
       draggedItemIndex === index ||
@@ -125,36 +97,24 @@ export class DashboardComponent implements AfterViewInit {
     ) {
       return;
     }
-    const countUpOrDown = index < draggedItemIndex ? -1 : 1;
 
-    for (let i = draggedItemIndex; i !== index; i += countUpOrDown) {
-      tempArray[i] = tempArray[i + countUpOrDown];
-    }
-    tempArray[index] = draggedWidgetConfig;
-    if (position < 0) {
-      const currentIndex = tempArray.indexOf(itemToBeReplaced);
-      tempArray[index] = itemToBeReplaced;
-      tempArray[currentIndex] = draggedWidgetConfig;
-    }
+    // Remove drag item from array
+    tempArray.splice(draggedItemIndex, 1);
+    // calculate adjusted destination location
+    const destIndex = index + Math.max(0, position);
+    const adjustedDestIndex =
+      destIndex + (destIndex > draggedItemIndex ? -1 : 0);
+    // insert dragged item at destination
+    tempArray.splice(adjustedDestIndex, 0, draggedWidgetConfig);
+    // update config array
     this.widgetConfigs = [...tempArray];
   }
 
-  setPlaceholderWidths(before: number | string, after: number | string): void {
-    this.placeholderBeforeElement.nativeElement.setAttribute(
-      'style',
-      `--size: ${before}`
-    );
-    this.placeholderAfterElement.nativeElement.setAttribute(
-      'style',
-      `--size: ${after}`
-    );
-  }
-
-  public setFilterState(state: FilterDateState) {
+  setFilterState(state: FilterDateState) {
     this.filterStore.setDates(state.startDate, state.endDate);
   }
 
-  public removeWidget(widgetId: string): void {
+  removeWidget(widgetId: string): void {
     this.widgetConfigs = this.widgetConfigs.filter((w) => w.id !== widgetId);
   }
 
@@ -164,25 +124,11 @@ export class DashboardComponent implements AfterViewInit {
   }
 
   onWidgetDragAction(index: number, event: WidgetDragActionEvent): void {
-    const colSize = CHART_WIDGET[this.draggedWidget].columnSpan;
-    switch (event.action) {
-      case WidgetDragAction.Enter:
-        this.injectPlaceholders(index, event.position);
-        if (event.position > 0) {
-          this.setPlaceholderWidths(0, colSize);
-        } else {
-          this.setPlaceholderWidths(colSize, 0);
-        }
-        break;
-      case WidgetDragAction.Change:
-        this.setPlaceholderWidths(
-          event.position < 0 ? colSize : 0,
-          event.position > 0 ? colSize : 0
-        );
-        break;
-      case WidgetDragAction.Cancel:
-        this.setPlaceholderWidths(0, 0);
-        break;
+    if (
+      event.action === WidgetDragAction.Enter ||
+      event.action === WidgetDragAction.Change
+    ) {
+      this.reArrangeWidgetItems(index, event.position);
     }
   }
 
@@ -203,7 +149,6 @@ export class DashboardComponent implements AfterViewInit {
         filters: widgetItem.widgetFilters,
         id: crypto.randomUUID(),
       };
-      this.setPlaceholderWidths(0, 0);
       this.widgetConfigs = [...this.widgetConfigs, widgetToAdd];
     }
   }
